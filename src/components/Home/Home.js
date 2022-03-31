29f9a304855736afca713c6dab074096cecd414d
Rasujon3 <rasujon3@gmail.com>
2022-03-31 21:56:06 +0600

    54-2 Recap Custom hook and load t-shirt data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "../Cart/Cart";
+import useTShirt from "../hooks/useTShirts";
+import TShirt from "../TShirt/TShirt";
+import "./Home.css";
 
 const Home = () => {
+  const [tShirts, setTShirts] = useTShirt();
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <div className="tshirt-container">
+        {tShirts.map((tShirt) => (
+          <TShirt key={tShirt._id} tShirt={tShirt} />
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart />
+      </div>
     </div>
   );
 };
